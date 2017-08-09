135ee27a8d73e0a409f3227eef97ee8f2630169a
liwei <liwei@dayang.com.cn>
2017-08-09 17:24:16 +0800

    JavaScript Utils
    JavaScript Utils for time handle

---
+++ TimeUtils/timeutil.js
@@ -0,0 +1,76 @@
+/**
+ * Created by liwei on 2017/8/3 0003.
+ */
+
+(function(context,methods){
+    context?context.TimeUtil= methods(): console.warn("Null Context!");
+}(window||this||self||global,function () {
+    return{
+        currentDate:new Date(),
+        getYear:function (date) {
+            if(date) {
+                return date.getFullYear();
+            }
+            return this.currentDate.getFullYear();
+        },
+        getMonth:function (date) {
+            if(!date){
+                date = this.currentDate;
+            }
+            //May be caused by Timezone, the general decrease 1
+            var month = date.getUTCMonth();
+            if(month==12){
+                return "1";
+            }else {
+                return month+1;
+            }
+        },
+        getDay:function (date) {
+            if(date){
+                return date.getDate();
+            }
+            return this.currentDate.getDate();
+        },
+        buildYMD:function(date){
+          if(!date){
+              date = this.currentDate;
+          }
+          return date.getYear()+"-"+date.getMonth()+"-"+date.getDay();
+        },
+        buildHMS:function (date) {
+            if(!date){
+                date = this.currentDate;
+            }
+            return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+        },
+        buildEntireDate:function (date) {
+            if(!date){
+                date = this.currentDate;
+            }
+            return this.getYear(date)+"-"+this.getMonth(date)+"-"+this.getDay(date)+" "+this.buildHMS(date);
+        },
+        buidNextDate:function (date) {
+            if(!date){
+                date = this.currentDate;
+            }
+            if(this.getMonth(date)=="12"){
+                return (this.getYear(date)+1)+"-"+this.getMonth(date)+"-"+this.getDay(date)+" "+this.buildHMS(date);
+            }else {
+                return this.getYear(date)+"-"+(this.getMonth(date)+1)+"-"+this.getDay(date)+" "+this.buildHMS(date);
+            }
+        },
+        buildPreviousDate:function(date){
+          if(!date){
+              date = this.currentDate;
+          }
+          if(this.getMonth(date)=="1"){
+              return (this.getYear(date)-1)+"-"+"12"+"-"+this.getDay(date)+" "+this.buildHMS(date);
+          }else {
+              return this.getYear(date)+"-"+(this.getMonth(date)-1)+"-"+this.getDay(date)+" "+this.buildHMS(date);
+          }
+        },
+        buildPrime:function () {
+            return new Date(arguments);
+        }
+    }
+}));
