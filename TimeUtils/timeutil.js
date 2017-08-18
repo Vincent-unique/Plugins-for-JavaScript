953370748a0720d308e77879821ea69f604b7614
Vincent-unique <vincent_unique@163.com>
2017-08-18 17:29:43 +0800

    seperator
    modify for seperator

--- TimeUtils/timeutil.js
+++ TimeUtils/timeutil.js
@@ -39,11 +39,14 @@
             }
             return day;
         },
-        buildYMD:function(date){
+        buildYMD:function(date,seperator){
           if(!date){
               date = this.currentDate;
           }
-          return this.getYear(date)+"-"+this.getMonth(date)+"-"+this.getDay(date);
+          if(!seperator){
+              seperator = "-";
+          }
+          return this.getYear(date)+seperator+this.getMonth(date)+seperator+this.getDay(date);
         },
         buildHMS:function (date) {
             if(!date){
@@ -51,34 +54,79 @@
             }
             return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
         },
-        buildEntireDate:function (date) {
+        buildEntireDate:function (date,seperator) {
             if(!date){
                 date = this.currentDate;
             }
-            return this.getYear(date)+"-"+this.getMonth(date)+"-"+this.getDay(date)+" "+this.buildHMS(date);
+            if(!seperator){
+                seperator = "-";
+            }
+            return this.getYear(date)+seperator+this.getMonth(date)+seperator+this.getDay(date)+" "+this.buildHMS(date);
         },
-        buidNextDate:function (date) {
+
+        /**
+         * build next month date of the day
+         * @param date
+         * @param seperator
+         * @returns {string}
+         */
+        buidNextDate:function (date,seperator) {
             if(!date){
                 date = this.currentDate;
+            }
+            if(!seperator){
+                seperator = "-";
             }
             if(this.getMonth(date)==12){
-                return (this.getYear(date)+1)+"-"+this.getMonth(date)+"-"+this.getDay(date)+" "+this.buildHMS(date);
+                return (this.getYear(date)+1)+seperator+this.getMonth(date)+seperator+this.getDay(date)+" "+this.buildHMS(date);
             }else {
-                return this.getYear(date)+"-"+(this.getMonth(date)+1)+"-"+this.getDay(date)+" "+this.buildHMS(date);
+                return this.getYear(date)+seperator+(this.getMonth(date)+1)+seperator+this.getDay(date)+" "+this.buildHMS(date);
             }
         },
-        buildPreviousDate:function(date){
+        /**
+         * build previous month date of the day
+         * @param date
+         * @param seperator
+         * @returns {string}
+         */
+        buildPreviousDate:function(date,seperator){
           if(!date){
               date = this.currentDate;
           }
+            if(!seperator){
+                seperator = "-";
+            }
           if(this.getMonth(date)==1){
-              return (this.getYear(date)-1)+"-"+"12"+"-"+this.getDay(date)+" "+this.buildHMS(date);
+              return (this.getYear(date)-1)+seperator+"12"+seperator+this.getDay(date)+" "+this.buildHMS(date);
           }else {
-              return this.getYear(date)+"-"+(this.getMonth(date)-1)+"-"+this.getDay(date)+" "+this.buildHMS(date);
+              return this.getYear(date)+seperator+(this.getMonth(date)-1)+seperator+this.getDay(date)+" "+this.buildHMS(date);
           }
         },
+        // buildPreviousDatePreDay:function (date) {
+        //    var date = this.buildPreviousDate(date);
+        //     return this.buildEntireDate(new Date(new Date(date).getTime()-1000*60*60*24));
+        // },
         buildPrime:function () {
             return new Date(arguments);
+        },
+        /**
+         * @param date
+         * @returns {Date} next day of the date
+         */
+        nextDay:function (date) {
+            if(!date){
+                date = this.currentDate;
+            }
+            return new Date(new Date(date).getTime()+1000*60*60*24);
+        },
+        /**
+         * @returns {Date} previous day of the date
+         */
+        preDay:function (date) {
+            if(!date){
+                date = this.currentDate;
+            }
+            return new Date(new Date(date).getTime()-1000*60*60*24);
         }
     }
 }));
