ab8f2549c1f8009d70ce25fdf149014f53182bba
Vincent-unique <vincent_unique@163.com>
2017-09-14 15:35:06 +0800

    perfect the components structure and reanme
    Signed-off-by: Vincent-unique <vincent_unique@163.com>

---
+++ Component/components.js
@@ -0,0 +1,71 @@
+/**
+ * Created by vincent on 2017/9/14 0014.
+ */
+
+/**
+ * Structure for ECMAScript plugins' developing
+ */
+
+/** @deprecated
+ * @description this structure is not best .
+ */
+(function(context,components){
+    !context ? (function () {
+        throw "Not found the runtime context.";
+    })():(
+        function (coms) {
+            if(coms&&coms.length>0){
+                coms.forEach(function (item, index, array) {
+                    Object.assign(context,item);
+                })
+            }
+        }
+    )(components);
+
+})(window||self||this||global,(function(){
+
+    return [
+        {
+            empty : null,
+            current:function () {
+               return new Date();
+            }
+        },
+        {
+            sorry:function () {
+                console.log("I am sorry.");
+            }
+        }
+    ]
+})())
+
+/**
+ * @primary
+ * Recommend to apply this components' structure.
+ * It will be easy extended into other runtime context and add other components
+ */
+(function (context,componets) {
+    !context ? (function () {
+        throw "Not found the runtime context.";
+    })():(
+       function (cxt) {
+           /**
+            * Initilize for all components
+            */
+           componets(cxt);
+           cxt.register = function(name,componet){
+               cxt[name] = componet;
+           };
+       }
+    )(context);
+})(window||this||self||global,function (cxt) {
+    Object.assign(cxt,{
+        component1:function () {
+
+        },
+        component2:function () {
+
+        },
+        empty:null
+    })
+})
